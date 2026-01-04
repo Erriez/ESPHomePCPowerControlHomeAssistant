4dd5d0a1cb1b5362864f790a8ec7cd30352b98f3
Erriez <Erriez@users.noreply.github.com>
2026-01-04 18:56:15 +0100

    Update copyright header

--- scripts/nas-power.py
+++ scripts/nas-power.py
@@ -2,7 +2,7 @@
 
 # MIT License
 # 
-# Copyright (c) 2020-2025 Erriez
+# Copyright (c) 2020-2026 Erriez
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
